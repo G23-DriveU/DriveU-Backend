282aa5520146fac20972cbd717a7a41d0c8eb812
szarobsky <szarob@me.com>
2024-11-09 17:34:42 -0500

    refined api responses

--- server.js
+++ server.js
@@ -45,15 +45,17 @@ app.get('/', (req, res) => {
 //GET users will take in firebaseUid and send user info from database to client.
 app.get('/users', async (req, res) => {
     console.log("GET USERS: ", req.query);
+    let response = {};
     try {
-        let result = {};
-        result.deviceId = await updateDeviceId(req.query.firebaseUid, req.query.deviceId);
-        result.user = await findUser(req.query.firebaseUid)
-        
-        res.json(result);
+        await updateDeviceId(req.query.firebaseUid, req.query.deviceId);
+        response.user = await findUser(req.query.firebaseUid)
+        response.status = "OK";
+        res.json(response);
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("GET USER ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
@@ -65,23 +67,38 @@ app.post('/users', async (req, res) => {
     let curUser = null;
     if (req.query.driver == 'true') curUser = new Driver(req.query);
     else curUser = new User(req.query);
+    let response = {};
 
     try {
         //If the user does not exist, it is inserted into the database.
         if (await doesUserExist(curUser.firebaseUid) == false) {
             result = await insertUser(curUser);
-            res.status(201).json(result);
+            if (result.rowCount == 0) {
+                response.status = "ERROR";
+                response.error = "User not inserted into database";
+                res.status(500).json(response);
+            }
+            else {
+                response.user = result.rows[0];
+                response.status = "OK";
+                res.status(201).json(response);
+            }
         }
         else {
             console.log("User already exists");
-            res.status(409).send('User already exists');
+            response.status = "CONFLICT";
+            response.conflict = "User already exists";
+            res.status(409).json(response);
         }
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("POST USER ERROR: ", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
+/*
 //DOESNT WORK
 app.get('/trips', async (req, res) => {
     console.log("GET TRIP: ", req.query);
@@ -107,22 +124,30 @@ app.get('/trips', async (req, res) => {
         res.status(500).send(error);
     }
 });
+*/
 
 //GET futureTrips will take in driverId and send all future trips for that driver to client.
 app.get('/futureTrips', async (req, res) => {
     console.log("GET FUTURE TRIPS: ", req.query);
+    let response = {};
     try {
         let result = await findFutureTrips(req.query.driverId);
-        res.json(result);
+        response.count = result.rowCount;
+        response.items = result.rows;
+        response.status = "OK";
+        res.json(response);
     } catch (error) {  
+        response.status = "ERROR";
+        response.error = error;
         console.log("GET FUTURE TRIPS ERROR", error)
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //POST futureTrips will take in future trip info and insert into database, sending database response back to client.
 app.post('/futureTrips', async (req, res) => {
     console.log("POST FUTURE TRIPS: ", req.query);
+    let response = {};
     try {
         //The FutureTrip object is created.
         let newTrip = await FutureTrip.createFutureTrip(req.query);
@@ -136,90 +161,138 @@ app.post('/futureTrips', async (req, res) => {
         for (let i = 0; i < prevFutureTripsCount; i++) {
             if (prevFutureTrips[i].start_time <= newTrip.eta && prevFutureTrips[i].eta >= newTrip.startTime) {
                 console.log("Overlapping future trips");
-                res.status(409).send('Overlapping future trips');
+                response.status = "CONFLICT";
+                response.conflict = "Overlapping future trips";
+                res.status(409).json(response);
                 return;
             }
         }
 
         //The new FutureTrip is inserted into the database.
         result = await insertFutureTrip(newTrip);
-        res.status(201).json(result);
+        if (result.rowCount === 0) {
+            response.status = "ERROR";
+            response.error = "Failed to insert future trip";
+            console.log("INSERT FUTURE TRIP ERROR");
+            res.status(500).json(response);
+            return;
+        }
+        response.item = result.rows[0];
+        response.status = "OK";
+        res.status(201).json(response);
     } catch (error) {
         //The error is caught if there is an error in the API response.
         if (error.toString().trim() == "Error: Error in API response") {
-            res.status(404).send('No route found');
+            response.status = "ERROR";
+            response.error = "No route found";
+            res.status(404).json(response);
         }
         else {
+            response.status = "ERROR";
+            response.error = error;
             console.log("POST FUTURE TRIPS ERROR", error);
-            res.status(500).send(error);
+            res.status(500).json(response);
         }
     }
 });
 
 //DELETE futureTrips will take in futureTripId and delete the future trip from the database.
 app.delete('/futureTrips', async (req, res) => {
+    let response = {};
     console.log("DELETE FUTURE TRIPS: ", req.query);
     try {
-        let result = await deleteFutureTrip(req.query.futureTripId);
-        res.json(result);
+        await deleteFutureTrip(req.query.futureTripId);
+        response.status = "OK";
+        res.json(response);
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("DELETE FUTURE TRIPS ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //GET rideRequestsForTrip will take in futureTripId and send all ride requests for that trip to client.
 app.get('/rideRequestsForTrip', async (req, res) => {
     console.log("GET RIDE REQUESTS FOR TRIP: ", req.query);
+    let response = {};
     try {
         let result = await findRideRequestsForTrip(req.query.futureTripId);
-        res.json(result);
+        response.status = "OK";
+        response.items = result.rows;
+        response.count = result.rowCount;
+        res.json(response);
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("GET RIDE REQUESTS ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //GET rideRequestsForRider will take in riderId and send all ride requests for that rider to client.
 app.get('/rideRequestsForRider', async (req, res) => {
     console.log("GET RIDE REQUESTS FOR RIDER: ", req.query);
+    let response = {};
     try {
         let result = await findRideRequestsForRider(req.query.riderId);
-        res.json(result);
+        response.status = "OK";
+        response.items = result.rows;
+        response.count = result.rowCount;
+        res.json(response);
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("GET RIDE REQUESTS ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //POST rideRequests will take in ride request info and insert into database, sending database response back to client.
 app.post('/rideRequests', async (req, res) => {
     console.log("POST RIDE REQUESTS: ", req.query);
+    let response = {};
     try {
         let newRideRequest = await RideRequest.createRideRequest(req.query);
 
         //SEND NOTIFICATION TO DRIVER============================================================
 
         result = await insertRideRequest(newRideRequest);
-        res.status(201).json(result);
+        if (result.rowCount === 0) {
+            response.status = "ERROR";
+            response.error = "Failed to insert ride request";
+            console.log("INSERT RIDE REQUEST ERROR");
+            res.status(500).json(response);
+            return;
+        }
+        response.item = result.rows[0];
+        response.status = "OK";
+        res.status(201).json(response);
     } catch (error) {
         //The error is caught if the rider has already requested a ride.
         if (error.toString().trim() == "Error: Rider already requested ride") {
-            res.status(409).send('Rider already requested ride');
+            response.status = "CONFLICT";
+            response.conflict = "Rider already requested ride";
+            res.status(409).json(response);
             return;
         }
         //The error is caught if there is an error in the API response.
         if (error.toString().trim() == "Error: Error in API response") {
-            res.status(404).send('No route found');
+            response.status = "ERROR";
+            response.error = "No route found";
+            res.status(404).json(response);
         }
+        response.status = "ERROR";
+        response.error = error;
         console.log("POST RIDE REQUESTS ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //PUT acceptRideRequest will take in rideRequestId and check for overlapping accepted ride requests, then accept the ride request and set the future trip to full, finally sending a notification to the rider.
 app.put('/acceptRideRequest', async (req, res) => {
     console.log("PUT ACCEPT RIDE REQUEST: ", req.query);
+    let response = {};
     try {
         //The ride request and future trip are found.
         let rideRequest = await findRideRequest(req.query.rideRequestId);
@@ -236,8 +309,10 @@ app.put('/acceptRideRequest', async (req, res) => {
                 continue;
             }
             if (prevRideRequests[i].futureTrip.start_time <= futureTrip.eta && prevRideRequests[i].futureTrip.eta >= futureTrip.start_time && prevRideRequests[i].status != "pending") {
+                response.status = "CONFLICT";
+                response.conflict = "Rider has overlapping ride requests";
                 console.log("Overlapping ride requests");
-                res.status(409).send('Overlapping ride requests');
+                res.status(409).json(response);
                 return;
             }
         }
@@ -245,46 +320,75 @@ app.put('/acceptRideRequest', async (req, res) => {
         //The ride request is accepted and the future trip is set to full.
         let result = {};
         result.rideRequest = await acceptRideRequest(req.query.rideRequestId);
+        if (result.rideRequest.rowCount === 0) {
+            response.status = "ERROR";
+            response.error = "Failed to accept ride request";
+            console.log("ACCEPT RIDE REQUEST ERROR");
+            res.status(500).json(response);
+            return;
+        }
         result.futureTrip = await setFutureTripFull(futureTrip.id);
+        if (result.futureTrip.rowCount === 0) {
+            response.status = "ERROR";
+            response.error = "Failed to set future trip to full";
+            console.log("SET FUTURE TRIP FULL ERROR");
+            res.status(500).json(response);
+            return;
+        }
 
         //SEND NOTIFICATION TO RIDER============================================================
 
-        res.status(201).json(result);
+        //The response is sent to the client.
+        response.status = "OK";
+        response.rideRequest = result.rideRequest.rows[0];
+        response.futureTrip = result.futureTrip.rows[0];
+        res.status(201).json(response);
     } catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("ACCEPT RIDE REQUEST ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //DELETE rideRequestsByRider will take in rideRequestId and delete from database.
 app.delete('/rideRequestsByRider', async (req, res) => {
     console.log("DELETE RIDE REQUEST BY RIDER: ", req.query);
+    let response = {};
     try {
-        let result = await deleteRideRequest(req.query.rideRequestId);
-        res.status(201).json(result);
+        await deleteRideRequest(req.query.rideRequestId);
+        response.status = "OK";
+        res.json(response);
     }
     catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("DELETE RIDE REQUEST ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
 //DELETE rideRequestsByDriver (reject) will take in rideRequestId, delete from database, and notify the rider.
 app.delete('/rideRequestsByDriver', async (req, res) => {
     console.log("DELETE RIDE REQUEST BY DRIVER: ", req.query);
+    let response = {};
     try {
         let result = await deleteRideRequest(req.query.rideRequestId);
 
         //SEND NOTIFICATION TO RIDER============================================================
 
-        res.status(201).json(result);
+        response.status = "OK";
+        res.json(response);
     }
     catch (error) {
+        response.status = "ERROR";
+        response.error = error;
         console.log("DELETE RIDE REQUEST ERROR", error);
-        res.status(500).send(error);
+        res.status(500).json(response);
     }
 });
 
+/*
 //DOESNT WORK
 app.post('/trips', async (req, res) => {
     console.log("POST TRIP: ", req.query);
@@ -349,6 +453,7 @@ app.post('/trips', async (req, res) => {
         res.status(500).send(error);
     }
 });
+*/
 
 //PAYPAL FUNCTIONALITY
 //The server will redirect to the paypal payment page.
