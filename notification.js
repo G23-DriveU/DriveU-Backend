ca33863be66d70ced953e63d1abc92b41c15b6ba
Julian Mendis <pasas.mendis@gmail.com>
2025-03-17 15:41:52 -0400

    added schedule notification endpoint in notification.js

--- notification.js
+++ notification.js
@@ -3,6 +3,7 @@
 //The necessary imports are included.
 const admin = require("firebase-admin")
 const serviceAccount = require("./driveu-ee379-firebase-adminsdk-anqjy-2d2d762dcc.json")
+const cron = require("node-cron")
 
 //The Firebase app is initialized.
 admin.initializeApp({
@@ -23,5 +24,31 @@ const sendNotification = async (title, body, token) => {
     return response;
 }
 
+const scheduleNotification = async (token, title, body, cronTime) => {
+    const message = {
+        notification:{
+            title: title,
+            body: body,
+
+        },
+        token: token,
+    };
+
+    console.log(`Scheduling a job with cronTime: ${cronTime}`);
+
+    const job = cron.schedule(cronTime, async () => {
+        console.log('Cron job triggered'); // Debugging log
+        try {
+            const response = await admin.messaging().send(message);
+            console.log(`Notification sent successfully: ${response.successCount} messages sent, ${response.failureCount} messages failed`);
+        } catch (error) {
+            console.error(`Error sending notification: ${error}`);
+        }
+        job.stop(); // Stop the job after it runs
+    });
+
+    console.log('Cron job scheduled'); // Debugging log
+};
+
 //The function is exported.
-module.exports = { sendNotification };
+module.exports = { sendNotification, scheduleNotification };
